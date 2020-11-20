e67e3e6aeef4832e85829ef25ac68602c2e69ff3
sonnylmao <69658757+sonnylmao@users.noreply.github.com>
2020-11-20 17:44:15 +0800

    Add files via upload

---
+++ classes3.py
@@ -0,0 +1,15 @@
+#static method - not changing
+
+class Math:
+    @staticmethod
+    def add5(x):
+        return x + 5
+    @staticmethod
+    def add10(x):
+        return x + 10
+    @staticmethod
+    def pr():
+        print("run")
+
+print(Math.add5(5))
+Math.pr()
