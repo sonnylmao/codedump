04504f8173c4f23f6b9f07c4150733fc928fae6a
sonnylmao <69658757+sonnylmao@users.noreply.github.com>
2021-01-15 18:18:43 +0800

    Create variables.java

---
+++ variables.java
@@ -0,0 +1,17 @@
+package firstproject;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int x = 5; //int
+		double y = 5.29; //float
+		boolean z = false; //boolean
+		char w = 'p'; //char with ''
+		String sc = "hello world!"; //string with ""
+		
+		System.out.println(z);
+    
+    
+	}
+
+}
